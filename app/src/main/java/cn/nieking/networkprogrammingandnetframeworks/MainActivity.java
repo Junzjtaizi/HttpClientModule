efeb9d770664c4f958f4fa352f31f7861c52f6d5
NieKing <jzjtaizi@qq.com>
2017-11-11 14:54:21 +0800

    First commit.

---
+++ app/src/main/java/cn/nieking/networkprogrammingandnetframeworks/MainActivity.java
@@ -0,0 +1,129 @@
+package cn.nieking.networkprogrammingandnetframeworks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpVersion;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+import org.apache.http.params.HttpProtocolParams;
+import org.apache.http.protocol.HTTP;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                userHttpClientPost("http://ip.taobao.com/service/getIpInfo.php");
+            }
+        }).start();
+    }
+
+    // 创建 HttpClient
+    private HttpClient createHttpClient() {
+        HttpParams mDefaultHttpParams = new BasicHttpParams();
+        // 设置链接超时
+        HttpConnectionParams.setConnectionTimeout(mDefaultHttpParams, 15000);
+        // 设置请求超时
+        HttpConnectionParams.setSoTimeout(mDefaultHttpParams, 15000);
+        HttpConnectionParams.setTcpNoDelay(mDefaultHttpParams, true);
+        HttpProtocolParams.setVersion(mDefaultHttpParams, HttpVersion.HTTP_1_1);
+        HttpProtocolParams.setContentCharset(mDefaultHttpParams, HTTP.UTF_8);
+        // 持续握手
+        HttpProtocolParams.setUseExpectContinue(mDefaultHttpParams, true);
+        HttpClient mHttpClient = new DefaultHttpClient(mDefaultHttpParams);
+        return mHttpClient;
+    }
+
+    /**
+     * HttpClient GET
+     * @param url
+     */
+    private void useHttpClientGet(String url) {
+        HttpGet mHttpGet = new HttpGet(url);
+        mHttpGet.addHeader("Connection", "Keep-Alive");
+        try {
+            HttpClient mHttpClient = createHttpClient();
+            HttpResponse mHttpResponse = mHttpClient.execute(mHttpGet);
+            HttpEntity mHttpEntity = mHttpResponse.getEntity();
+            int code = mHttpResponse.getStatusLine().getStatusCode();
+            if (null != mHttpEntity) {
+                InputStream mInputStream = mHttpEntity.getContent();
+                String response = converStreamToString(mInputStream); // 1
+                Log.d(TAG, "请求状态码：" + code +"\n请求结果：\n" + response);
+                mInputStream.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * HttpClient POST
+     * @param url
+     */
+    private void userHttpClientPost(String url) {
+        HttpPost mHttpPost = new HttpPost(url);
+        mHttpPost.addHeader("Connection", "Keep-Alive");
+        try {
+            HttpClient mHttpClient = createHttpClient();
+            List<NameValuePair> postParams = new ArrayList<>();
+            // 要传递的参数
+            postParams.add(new BasicNameValuePair("ip", "59.108.54.37"));
+            mHttpPost.setEntity(new UrlEncodedFormEntity(postParams));
+            HttpResponse mHttpResponse = mHttpClient.execute(mHttpPost);
+            HttpEntity mHttpEntity = mHttpResponse.getEntity();
+            int code = mHttpResponse.getStatusLine().getStatusCode();
+            if (null != mHttpEntity) {
+                InputStream mInputStream = mHttpEntity.getContent();
+                String response = converStreamToString(mInputStream);
+                Log.d(TAG, "请求状态码：" + code +"\n请求结果：\n" + response);
+                mInputStream.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * ToString
+     * @param is
+     * @return
+     * @throws IOException
+     */
+    private String converStreamToString(InputStream is) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuffer sb = new StringBuffer();
+        String line = null;
+        while ((line = reader.readLine()) != null) {
+            sb.append(line + "\n");
+        }
+        String response = sb.toString();
+        return response;
+    }
+}
